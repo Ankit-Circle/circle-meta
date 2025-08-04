7edc10c26f63d7fbec16715bcfc614d89d1b5245
Pranav Acharya <82842289+pranav-loops@users.noreply.github.com>
2025-08-04 12:15:10 -0700

    update meta tags

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -22,6 +22,8 @@ export const getServerSideProps: GetServerSideProps = async ({ params, req }) =>
     <!DOCTYPE html>
     <html lang="en">
     <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>${product.name}</title>
       <meta property="og:title" content="${product.name}" />
       <meta property="og:description" content="${product.description}" />
