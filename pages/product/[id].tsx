4fd2bde120ad7ab4e12f3b2d91d0ea0e732d5b71
Pranav Acharya <82842289+pranav-loops@users.noreply.github.com>
2025-08-04 11:48:53 -0700

    update env var

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -15,7 +15,7 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
       <meta property="og:title" content="${product.name}" />
       <meta name="description" property="og:description" content="${product.description}" />
       <meta name="image" property="og:image" content="${product.image_url}" />
-      <meta property="og:url" content="${process.env.NEXT_PUBLIC_APP_URL}/product/${product.product_url_key}" />
+      <meta property="og:url" content="${process.env.CIRCLE_APP_URL}/product/${product.product_url_key}" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content="${product.name}" />
       <meta name="twitter:description" content="${product.description}" />
@@ -23,7 +23,7 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
     </head>
     <body>
       <script>
-        window.location.href = "${process.env.NEXT_PUBLIC_APP_URL}/product/${product.product_url_key}";
+        window.location.href = "${process.env.CIRCLE_APP_URL}/product/${product.product_url_key}";
       </script>
     </body>
     </html>
